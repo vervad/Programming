0f9d91250503e505c1773e04426c481647bc9809
Vadim <vervad346@gmail.com>
2020-11-17 11:18:14 +0300

    23 done

--- Practice/23/C++/23.cpp
+++ Practice/23/C++/23.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <iomanip>
+#include "Factorial.h"
 #include "Taylor.h"
+#include "Combinations.h"
+using namespace std;
 
 int main()
 {
-	float n;
-	int k;
-	std::cin >> n;
-	std::cin >> k;
-	std::cout << sin(n, k);
+	cout << "n" << "\t" << "n!" << "\n";
+	for (int n = 1; n <= 10; n++)
+	{
+		cout << n << "\t" << fact(n) << "\n";
+	}
+	cout << "\n";
+
+	cout << "x" << "\t" << "sin(x)" << "\n";
+	for (float x = 0; x <= 3.1415/4; x += 3.1415/180)
+	{
+		cout << x << "\t" << setprecision(4) << sin(x, 5) << "\n";
+	}
+	cout << "\n";
+
+	cout << "k" << "\t" << "C(k, 10)" << "\n";
+	for (int k = 1; k <= 10; k++)
+	{
+		cout << k << "\t" << comb(k, 10) << "\n";
+	}
 }
--- Practice/23/C++/Combinations.h
+++ Practice/23/C++/Combinations.h
@@ -2,5 +2,9 @@
 #define COMBINATIONS_H
 #include "Factorial.h"
 
-
+int comb(int k, int n)
+{
+	int result = fact(n) / (fact(k) * fact(n - k));
+	return result;
+}
 #endif
